f93d87b87881565a587cd7ae3f63b0baf1b04431
Rodrigo <rodrigo.queiroz.neves@gmail.com>
2023-09-13 14:00:41 -0300

    chore: Production config

--- src/env/index.ts
+++ src/env/index.ts
@@ -6,8 +6,9 @@ else config({ path: '.env' })
 
 const eventSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3333),
+  PORT: z.coerce.number().default(3333),
 })
 
 const _env = eventSchema.safeParse(process.env)
